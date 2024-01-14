0fe54019aa5ed183da8bb1d6f9714c8b385498c0
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-01-14 15:19:45 -0500

    Update convert⁄fast_voice_clone.py added notes to it
    just notes for the feature im currently working on where im trying to also get every model in coqui as a voice cloning option

--- convert⁄fast_voice_clone.py
+++ convert⁄fast_voice_clone.py
@@ -20,3 +20,31 @@ tts.tts_with_vc_to_file(
     file_path="output.wav",
     speaker = "p363"
 )
+
+
+'''thsi is way better make a dictionary of all of the modesl hat are fast in coqio to select from for the speaker value, for them
+and the spaeker value will be None if it isnt a ulti voice one, whihc is most of them
+BUT if it does then you can set it to seom voice within the three that do so that w=should make the if statment a lot shorter
+
+so then you can just amke it 
+
+make a disctonary of the speakers to set for each model witht eh rtules above
+
+fast_voice_cloning_models= "get mdoels command"
+fast_voice_cloning_models_dict= {}
+
+if the model selected for voice cloning is one fo the fast ones then:
+
+tts = TTS(model_selected)
+
+target_speaker_wav = "drew.wav"
+
+# First text to speech conversion
+tts.tts_with_vc_to_file(
+    "An adventure begins at the break of dawn.",
+    speaker_wav=target_speaker_wav,
+    file_path="output.wav",
+    speaker = fast_voice_cloning_models_dict[model_selected] #this will make it select a specific voice in the dict assosicated witht hat model or just None 
+    #speaker="p363"
+)
+'''
