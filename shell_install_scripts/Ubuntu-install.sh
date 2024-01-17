37209a5d5838cac78abfa466c6eb76791f9afb17
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-01-16 22:43:52 -0500

    Update Ubuntu-install.sh updating install script...
    idk trying to improve upon it

--- shell_install_scripts/Ubuntu-install.sh
+++ shell_install_scripts/Ubuntu-install.sh
@@ -2,13 +2,38 @@
 
 # Check if the script is running on Linux
 if [ "$(uname)" == "Linux" ]; then
-    # Update package lists and install Calibre
-    sudo apt-get update
-    sudo apt-get install calibre
 
-    # Create and activate a new Conda environment
+    # Update package lists
+    sudo apt-get update
+
+    # Check for and install pip if needed
+    if ! command -v pip &> /dev/null; then
+        sudo apt-get install python3-pip
+    fi
+
+    # Check for Python 3.10 or later and install if needed
+    python_version=$(python3 --version 2>&1)
+    if [[ ! "$python_version" =~ ^Python\ (3\.1[0-9]|3\.[2-9]\.|[4-9]\.) ]]; then
+        sudo apt-get install python3.10
+    fi
+
+    # Check for Conda and install if needed
+    if ! command -v conda &> /dev/null; then
+        # Download and install Miniconda
+        miniconda_installer="Miniconda3-latest-Linux-x86_64.sh"
+        wget https://repo.anaconda.com/miniconda/$miniconda_installer
+        bash $miniconda_installer -b -p ~/miniconda
+        rm $miniconda_installer
+        # Add Conda to PATH
+        export PATH="$HOME/miniconda/bin:$PATH"
+    fi
+
+    # Create and activate a new Conda environment using Python 3.10
     conda create --name VoxNovel python=3.10
     conda activate VoxNovel
+
+    # Install Calibre
+    sudo apt-get install calibre
 
     # Clone the VoxNovel repository
     git clone https://github.com/DrewThomasson/VoxNovel.git
@@ -17,9 +42,7 @@ if [ "$(uname)" == "Linux" ]; then
     cd VoxNovel
 
     # Install necessary Python packages
-    pip install styletts2
-    pip install tts==0.21.3
-    pip install booknlp
+    pip install styletts2 tts==0.21.3 booknlp
     pip install -r Ubuntu_requirements.txt
 
     # Download the spaCy language model
