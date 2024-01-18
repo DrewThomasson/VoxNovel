3986547b80c8f05a8e5a2597f0d8438760df0ba1
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-01-17 22:00:58 -0500

    Update Ubuntu_run_voxnovel.sh added other possible miniconda paths
    Update Ubuntu_run_voxnovel.sh added other possible miniconda paths idk i had a computer that had it as minidona3 for some reason so yeh

--- shell_install_scripts/run/Ubuntu_run_voxnovel.sh
+++ shell_install_scripts/run/Ubuntu_run_voxnovel.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Activate the Conda environment
-source ~/miniconda/bin/activate VoxNovel
+# Iterate through common Miniconda paths
+for miniconda_path in "~/miniconda" "~/miniconda3" "~/opt/miniconda" "~/opt/miniconda3" "/opt/miniconda" "/opt/miniconda3"; do
+  if [[ -d $miniconda_path ]]; then
+    source "$miniconda_path/bin/activate" VoxNovel
+    break  # Exit the loop if a valid path is found
+  fi
+done
+
+if [[ "$CONDA_PREFIX" == "" ]]; then
+  echo "Error: Could not find Miniconda installation."
+  exit 1
+fi
 
 cd ~/VoxNovel
-
-# Run the Python script
 python gui_run.py
