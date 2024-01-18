a2143703ff069a373f3037d1c4a4dc89db81a971
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-01-17 22:57:56 -0500

    Update Ubuntu_run_voxnovel.sh fixed type
    now it should work

--- shell_install_scripts/run/Ubuntu_run_voxnovel.sh
+++ shell_install_scripts/run/Ubuntu_run_voxnovel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Iterate through common Miniconda paths
-for miniconda_path in "~/miniconda" "~/miniconda3" "~/opt/miniconda" "~/opt/miniconda3" "/opt/miniconda" "/opt/miniconda3"; do
+for miniconda_path in ~/miniconda ~/miniconda3 ~/opt/miniconda ~/opt/miniconda3 /opt/miniconda /opt/miniconda3; do
   if [[ -d $miniconda_path ]]; then
     source "$miniconda_path/bin/activate" VoxNovel
     break  # Exit the loop if a valid path is found
