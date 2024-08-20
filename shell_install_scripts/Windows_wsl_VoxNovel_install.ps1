ba3e9209b968bdc47a29ebe0c229f3f1be9cecf0
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-08-20 16:12:08 -0700

    Update Windows_wsl_VoxNovel_install.ps1

--- shell_install_scripts/Windows_wsl_VoxNovel_install.ps1
+++ shell_install_scripts/Windows_wsl_VoxNovel_install.ps1
@@ -1,6 +1,6 @@
 # Define variables
 $desktopPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'VoxNovel.lnk')
-$scriptPath = "https://github.com/DrewThomasson/VoxNovel/blob/main/shell_install_scripts/Windows_wsl_run_VoxNovel.ps1"
+$scriptPath = "https://raw.githubusercontent.com/DrewThomasson/VoxNovel/main/shell_install_scripts/Ubuntu-install.sh"
 $logoUrl = "https://github.com/DrewThomasson/VoxNovel/raw/6f49c6a8b36927c987b1d628ff3e9c1afcb04dab/readme_files/logo.jpeg"
 $shortcutName = "VoxNovel"
 
@@ -9,7 +9,7 @@ Write-Host "Creating desktop shortcut..."
 $WshShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($desktopPath)
 $Shortcut.TargetPath = "powershell.exe"
-$Shortcut.Arguments = "-ExecutionPolicy Bypass -File $scriptPath"
+$Shortcut.Arguments = "-ExecutionPolicy Bypass -NoNewWindow -Command wsl -d Ubuntu -- bash -c 'wget -O - $scriptPath | bash'"
 $Shortcut.IconLocation = "powershell.exe,0" # Default PowerShell icon
 $Shortcut.Save()
 
@@ -23,20 +23,8 @@ Write-Host "Setting icon for the shortcut..."
 $Shortcut.IconLocation = $logoPath
 $Shortcut.Save()
 
-# Run WSL installation
-Write-Host "Starting WSL installation..."
-Start-Process powershell -ArgumentList "wsl --install Ubuntu" -NoNewWindow -Wait
-
-# Wait for WSL installation to complete (adjust time if needed)
-Write-Host "Waiting for WSL installation to complete. This may take some time..."
-Start-Sleep -Seconds 60
-
 # Ensure WSL is set up and not prompting interactively
 Write-Host "Ensuring WSL is set up and not prompting interactively..."
 wsl --list --verbose
-
-# Run the installation script in the WSL environment
-Write-Host "Running the installation script in WSL..."
-Start-Process powershell -ArgumentList "wsl -d Ubuntu -- bash -c 'yes | wget -O - https://raw.githubusercontent.com/DrewThomasson/VoxNovel/main/shell_install_scripts/Ubuntu-install.sh | bash'" -NoNewWindow -Wait
 
 Write-Host "Process completed."
