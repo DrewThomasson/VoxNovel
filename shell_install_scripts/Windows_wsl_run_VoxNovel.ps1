8c0cd8631c7a7380866860de828cb3c34a365192
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-08-20 15:58:42 -0700

    Update Windows_wsl_run_VoxNovel.ps1

--- shell_install_scripts/Windows_wsl_run_VoxNovel.ps1
+++ shell_install_scripts/Windows_wsl_run_VoxNovel.ps1
@@ -1,1 +1,7 @@
 
+# Run WSL with specified commands
+$wslCommand = @"
+wsl -d Ubuntu bash -c "conda activate VoxNovel && cd ~ && cd VoxNovel && python gui_run.py"
+"@
+
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `$wslCommand" -NoNewWindow -Wait
