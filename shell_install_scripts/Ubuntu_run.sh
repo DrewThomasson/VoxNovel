8fc87ddda8e3726665c38890975a6ec055cf9c89
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-08-20 19:07:10 -0700

    Update Ubuntu_run.sh

--- shell_install_scripts/Ubuntu_run.sh
+++ shell_install_scripts/Ubuntu_run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # Activate the Conda environment
-source /home/drew/miniconda/etc/profile.d/conda.sh
+CONDA_PATH=$(dirname $(which conda))
+source $CONDA_PATH/../etc/profile.d/conda.sh
 conda activate VoxNovel
 
 # Navigate to the VoxNovel directory
-cd ~/VoxNovel
+VENV_DIR=$(conda info --envs | grep VoxNovel | awk '{print $NF}')
+cd $VENV_DIR
 
 # Run the Python script
 python gui_run.py
