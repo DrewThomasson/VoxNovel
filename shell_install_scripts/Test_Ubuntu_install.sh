d2133a7c4aa725ccd21905604a35aa8f0ce21408
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-08-22 12:15:05 -0700

    Update Test_Ubuntu_install.sh fixed typo in destination address

--- shell_install_scripts/Test_Ubuntu_install.sh
+++ shell_install_scripts/Test_Ubuntu_install.sh
@@ -4,7 +4,7 @@
 USER_HOME=$(eval echo ~${SUDO_USER:-$USER})
 
 # Set the destination directory and file URL
-DEST_DIR="$USER_HOME/.local/share/tts"
+DEST_DIR="$USER_HOME/.local/share/tts/tts_models--multilingual--multi-dataset--xtts_v2"
 FILE_URL="https://github.com/DrewThomasson/VoxNovel/raw/main/readme_files/tos_agreed.txt"
 
 # Create the destination directory if it doesn't exist
