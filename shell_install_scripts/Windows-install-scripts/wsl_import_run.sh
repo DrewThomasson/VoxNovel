ee6ce99397d32f31a4cdf25673bca4281da8ef06
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-08-22 05:33:02 -0700

    Create wsl_import_run.sh

---
+++ shell_install_scripts/Windows-install-scripts/wsl_import_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Switch to the appropriate user
+su - drew
+
+echo "Conda path: $(which conda)"
+echo "Available environments:"
+conda env list
+
+echo "Activating environment..."
+source ~/miniconda/etc/profile.d/conda.sh
+conda activate VoxNovel
+
+echo "Current environment: $CONDA_DEFAULT_ENV"
+
+cd ~/VoxNovel
+
+echo "Running Python script..."
+cd ~ && cd VoxNovel && python gui_run.py
