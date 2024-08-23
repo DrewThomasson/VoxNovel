d1b9c16c910f283791a942f856365474c6ef0696
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-08-23 12:36:32 -0400

    Update Ubuntu-install.sh pre-download the tos_agreed.txt file so then you don't have to type yes in the terminal when downloading the coqio xtts_v2 model
    pre-download the tos_agreed.txt file so then you don't have to type yes in the terminal when downloading the coqio xtts_v2 model

--- shell_install_scripts/Ubuntu-install.sh
+++ shell_install_scripts/Ubuntu-install.sh
@@ -122,7 +122,24 @@ if [ "$(uname)" == "Linux" ]; then
     # Update the application database
     sudo update-desktop-database
 
+    # This part of the script will pre-download the tos_agreed.txt file so then you don't have to type yes in the terminal when downloading the coqio xtts_v2 model. 
+    # Get the current user's home directory
+    USER_HOME=$(eval echo ~${SUDO_USER:-$USER})
     
+    # Set the destination directory and file URL
+    DEST_DIR="$USER_HOME/.local/share/tts/tts_models--multilingual--multi-dataset--xtts_v2"
+    FILE_URL="https://github.com/DrewThomasson/VoxNovel/raw/main/readme_files/tos_agreed.txt"
+    
+    # Create the destination directory if it doesn't exist
+    mkdir -p "$DEST_DIR"
+    
+    # Download the file to the destination directory
+    curl -o "$DEST_DIR/tos_agreed.txt" "$FILE_URL"
+    
+    echo "File has been saved to $DEST_DIR/tos_agreed.txt"
+    echo "The tos_agreed.txt file is so that you don't have to tell coqio tts yes when downloading the xtts_v2 model."
+
+
 else
     echo "This script is intended to be run on Linux."
 fi
