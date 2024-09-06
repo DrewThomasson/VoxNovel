808f6381bca47b799622ee8fdbffffe441d4af4a
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-09-06 00:54:11 -0400

    Update Steam-Deck-Run-VoxNovel.sh now one-liner command in .sh file

--- shell_install_scripts/run/Steam-Deck-Run-VoxNovel.sh
+++ shell_install_scripts/run/Steam-Deck-Run-VoxNovel.sh
@@ -1,18 +1,3 @@
-cd ~
-
-#Initalize homebrew env if not done so yet
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-conda activate VoxNovel
-
-cd VoxNovel
-
-git pull
-
-python --version
-
-python run gui_run.py
-
-xdg-open ~/VoxNovel/output_audiobooks/
+cd ~ && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && conda activate VoxNovel && cd VoxNovel && git pull && python --version && python run gui_run.py && xdg-open ~/VoxNovel/output_audiobooks/
 
 
