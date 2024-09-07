037727a5700e29f303740e890810781dd22451fa
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-09-07 13:35:13 -0400

    Update Steam-Deck-Run-VoxNovel.sh fixed added all initalizations brew and conda

--- shell_install_scripts/run/Steam-Deck-Run-VoxNovel.sh
+++ shell_install_scripts/run/Steam-Deck-Run-VoxNovel.sh
@@ -1,3 +1,45 @@
 #!/bin/bash
+
+# >>> conda initialize >>>
+echo "Activating conda..."
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/drew/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/drew/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/drew/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/drew/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+echo "conda activated!"
+# <<< conda initialize <<<
+
+# Activate Homebrew
+echo "Activating brew..."
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "Brew activated!"
+
 echo "Starting..."
-echo "Activating brew..." && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && echo "Activated Brew! launching VoxNovel..." && cd ~ && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && conda activate VoxNovel && cd VoxNovel && git pull && python --version && python gui_run.py && xdg-open ~/VoxNovel/output_audiobooks/
+
+# Change to home directory
+cd ~
+
+conda env list
+
+# Activate Conda environment
+conda activate VoxNovel
+
+# Navigate to VoxNovel directory
+cd VoxNovel
+
+# Pull latest changes from the repository
+git pull
+
+# Display Python version
+python --version
+
+# Run the VoxNovel GUI
+python gui_run.py && xdg-open ~/VoxNovel/output_audiobooks
