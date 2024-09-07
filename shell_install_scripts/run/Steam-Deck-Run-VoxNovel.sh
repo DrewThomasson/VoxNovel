05759f73a640bc6dd17d5f70ab6e9a3454dfbe94
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-09-07 02:14:37 -0400

    Update Steam-Deck-Run-VoxNovel.sh added bash at top of it

--- shell_install_scripts/run/Steam-Deck-Run-VoxNovel.sh
+++ shell_install_scripts/run/Steam-Deck-Run-VoxNovel.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+echo "Starting..."
 echo "Activating brew..." && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && echo Activated Brew! launching VoxNovel..." && cd ~ && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && conda activate VoxNovel && cd VoxNovel && git pull && python --version && python run gui_run.py && xdg-open ~/VoxNovel/output_audiobooks/
