47eda0973c4c74449ffdaffb2490ef6879447f49
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-09-06 17:24:55 -0700

    Update Steam-Deck-Run-VoxNovel.sh updated for nix

--- shell_install_scripts/run/Steam-Deck-Run-VoxNovel.sh
+++ shell_install_scripts/run/Steam-Deck-Run-VoxNovel.sh
@@ -1,3 +1,59 @@
-cd ~ && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" && conda activate VoxNovel && cd VoxNovel && git pull && python --version && python run gui_run.py && xdg-open ~/VoxNovel/output_audiobooks/
+# Just to be safe drew youll probbaly have to run all these at the beginning of the VoxNovel Launch script
+# Activate nix Packages temporarily for the current session
+echo "Activating Nix Packages..."
+
+nix-shell -p calibre
+nix-shell -p glibc
+nix-shell -p gcc
+nix-shell -p ffmpeg
+nix-shell -p git
+nix-shell -p espeak
+nix-shell -p unzip
+nix-shell -p wget
+
+calibre --version
+gcc --version
+nix-shell -p glibc --run "ldd --version"
+ffmpeg --version
+git --version
+espeak --version
+unzip -v
+wget --version
+
+
+echo "Nix Packages Activated!"
+
+
+
+
+
+echo "Activating Miniconda in current session..."
+
+# Step 5: Initialize Conda for bash and zsh
+echo "Initializing Conda..."
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+
+# Step 6: Reload shell configuration
+echo "Reloading shell configuration..."
+source ~/.bashrc
+source ~/.zshrc
+
+conda --version
+
+echo "Miniconda Activated!"
+
+cd ~
+cd VoxNovel
+git pull
+conda activate VoxNovel
+python --version
+python run gui_run.py
+
+
+echo "Opening output_audiobooks folder in xdg..."
+xdg-open ~/VoxNovel/output_audiobooks/
+echo "Opened! You can view your finished audiobooks here!"
+echo "Complete!"
 
 
