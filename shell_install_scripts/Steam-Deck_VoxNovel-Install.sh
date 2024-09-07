b60a8bdaf39c48176a55fcc7a73b35abe6387443
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-09-06 17:17:11 -0700

    Update Steam-Deck_VoxNovel-Install.sh displays installed package versions at end and added not to self at end
    Update Steam-Deck_VoxNovel-Install.sh displays installed package versions at end and added not to self at end

--- shell_install_scripts/Steam-Deck_VoxNovel-Install.sh
+++ shell_install_scripts/Steam-Deck_VoxNovel-Install.sh
@@ -292,8 +292,59 @@ echo "You can manually delete the dektop shortcut if you want."
 
 
 
+#Just to be safe drew youll probbaly have to run all these at the beginning of the VoxNovel Launch script
+# Activate nix Packages temporarily for the current session
+#echo "Activating Nix Packages..."
+
+#nix-shell -p calibre
+#nix-shell -p glibc
+#nix-shell -p gcc
+#nix-shell -p ffmpeg
+#nix-shell -p git
+#nix-shell -p espeak
+#nix-shell -p unzip
+#nix-shell -p wget
+
+#calibre --version
+#gcc --version
+#ffmpeg --version
+#git --version
+#espeak --version
+#unzip -v
+#wget --version
+
+#echo "Nix Packages Activated!"
 
 
 
+
+
+#echo "Activating Miniconda in current session..."
+
+# Step 5: Initialize Conda for bash and zsh
+#echo "Initializing Conda..."
+#~/miniconda3/bin/conda init bash
+#~/miniconda3/bin/conda init zsh
+
+# Step 6: Reload shell configuration
+#echo "Reloading shell configuration..."
+#source ~/.bashrc
+#source ~/.zshrc
+#conda --version
+
+#echo "Miniconda Activated!"
+
+
+
+echo "Displaying installed package versions..."
+
+calibre --version
+gcc --version
+ffmpeg --version
+git --version
+espeak --version
+unzip -v
+wget --version
+conda --version
 
 echo "VoxNovel Install FINISHED! (You can close out of this window now)"
