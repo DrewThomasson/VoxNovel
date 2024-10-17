2796a31865016e436c777a41e4062d8d9cf62fdd
Drew Thomasson <126999465+DrewThomasson@users.noreply.github.com>
2024-10-17 13:37:05 -0700

    Update Ubuntu-install.sh

--- shell_install_scripts/Ubuntu-install.sh
+++ shell_install_scripts/Ubuntu-install.sh
@@ -53,7 +53,9 @@ if [ "$(uname)" == "Linux" ]; then
 
     # Install necessary Python packages
     pip install styletts2 
-    pip install tts==0.21.3
+    #pip install tts==0.21.3
+    # This is the updated tts install pip cause the official is dead now sad face
+    pip install coqui-tts
     pip install booknlp==1.0.7.1
     pip install bs4
     pip install -r Ubuntu_requirements.txt
